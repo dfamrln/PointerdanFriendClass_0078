629d07fe053780064f73344ba8aa192dd0d960b8
dfamrln <mhdffa1@gmail.com>
2025-05-06 17:40:47 +0700

    second commit

--- contoh.cpp
+++ contoh.cpp
@@ -25,8 +25,32 @@ class PersegiPanjang
 
 };
 
+class Lingkaran
+{
+    public:
+        int jarijari;
+
+        void input()
+        {
+            cout << "Masukkan Jari-jari : ";
+            cin >> jarijari;
+        }
+
+        int LuasLingkaran(int r)
+        {
+            return 3.14 * r * r;
+        }
+};
+
+int main()
+{
+    PersegiPanjang objekPP; // membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan Panjang : ";
+    cin >> objekPP.panjang;
     cout << "Masukkan Lebar : ";
     cin >> objekPP.lebar;
     cout << "Luas Persegi Panjang : " <<
-    objekPP.LuasPp(objekPP.panjang, objekPP.lebar) << endl;
+    objekPP.luasPp(objekPP.panjang, objekPP.lebar) << endl;
 }
